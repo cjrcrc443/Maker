20c74b7c5189aa8ef59ec227089a93c39445ae56
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-05-31 23:23:02 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,11 @@
+
+OWNER = ["IQ7amo"]
+OWNER_NAME = "﮼محمد"
+BOT_TOKEN = "6937321286:AAEacYTTdOlPww6OEP5penuymB7I5pjphwc"
+DATABASE = "mongodb+srv://Rekxarbot:mRsOv45okKDb3M6o@cluster0.wdnhkhn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+CHANNEL = "https://t.me/mgimt"
+GROUP = "https://t.me/iqsupp"
+VIDEO = "https://graph.org/file/3202937ba2792dfa8722f.jpg"
+VID_SO = "https://graph.org/file/3202937ba2792dfa8722f.jpg"
+LOGS = "IQSUPP"
+
---
+++ bot.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+from pyromod import listen
+
+
+
+bot = Client(
+    "mo",
+    api_id="12962251",
+    api_hash="b51499523800add51e4530c6f552dbc8",
+    bot_token="6937321286:AAEacYTTdOlPww6OEP5penuymB7I5pjphwc",
+    plugins=dict(root="Maker")
+    )
+
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
+    await bot.start()
+    Alina = "IQ7amo"
+    await bot.send_message(Alina, "**بە سەرکەوتوویی ڕێکخەری بۆت چالاکبوو 🥀،**")
+    print("[INFO]: ڕێکخەر چالاکبوو و نامەکەم نارد بۆ گەشەپێدەر ⚡🚦.")
+    await idle()
