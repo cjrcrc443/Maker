275f6a9dfcd53a9577ce687c50fe62acdca91d4e
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-06-01 00:20:49 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,11 +1,10 @@
 
 OWNER = ["Hawaall"]
-OWNER_NAME = "𝐇𝐚𝐰𝐚𝐥𝐥🔥 ꨄᵉⁿᵈ"ᨒ"
+OWNER_NAME = "𝐇𝐚𝐰𝐚𝐥𝐥🔥 ꨄᵉⁿᵈ""
 BOT_TOKEN = "7457379005:AAH5jd36IUUAVdtIx2iAjgy_WnrxRT59hbw"
-DATABASE = ""
+DATABASE = "mongodb+srv://Rekxarbot:mRsOv45okKDb3M6o@cluster0.wdnhkhn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 CHANNEL = "https://t.me/Haawall"
 GROUP = "https://t.me/pieceofsad0"
 VIDEO = "https://graph.org/file/c8d0d49f5e13290314807.jpg"
 VID_SO = "https://graph.org/file/c8d0d49f5e13290314807.jpg"
-LOGS = "Haawall"
-
+LOGS = "pieceofsad0"
