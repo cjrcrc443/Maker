656795d091c7b8dfb782342b420400ea0770c51a
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-07-12 22:24:35 +0300

    Update admin.py

--- ALINA/admin.py
+++ ALINA/admin.py
@@ -1,9 +1,9 @@
 import asyncio
 from config import OWNER, OWNER_NAME, VIDEO
 from pyrogram import Client, filters
-from SEMO.info import (remove_active, is_served_call, joinch)
-from SEMO.Data import (get_call, get_dev, get_group, get_channel)
-from SEMO.info import (add, db, download, gen_thumb)
+from ALINA.info import (remove_active, is_served_call, joinch)
+from ALINA.Data import (get_call, get_dev, get_group, get_channel)
+from ALINA.info import (add, db, download, gen_thumb)
 from pytgcalls import PyTgCalls, StreamType
 from pyrogram.enums import ChatType, ChatMemberStatus
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
