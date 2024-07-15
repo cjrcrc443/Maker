3818fd417357663390e23e89d68a48ae991f96a9
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-07-15 09:53:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,14 @@ bot = Client(
     "mo",
     api_id="12962251",
     api_hash="b51499523800add51e4530c6f552dbc8",
-    bot_token="6937321286:AAEh2B5dr2BFJBuRDa3Q5JKEoniXBOWfnic",
+    bot_token="7457379005:AAFGa-KqppzmKI1NuzW2sp2PUwJLmyC3wuE",
     plugins=dict(root="Maker")
     )
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
     await bot.start()
-    Alina = "IQ7amo"
+    Alina = "Hawaallll"
     await bot.send_message(Alina, "**بە سەرکەوتوویی ڕێکخەری بۆت چالاکبوو 🥀،**")
     print("[INFO]: ڕێکخەر چالاکبوو و نامەکەم نارد بۆ گەشەپێدەر ⚡🚦.")
     await idle()
