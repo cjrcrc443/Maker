1d3b766f6b6a3b0707918f4a97f7f0dbf7bb89e8
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-07-17 12:35:10 +0300

    Update admin.py

--- ALINA/admin.py
+++ ALINA/admin.py
@@ -5,8 +5,14 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import CallbackQuery
 from pyrogram.enums import ChatType, ChatMemberStatus
 
-from pytgcalls import PyTgCalls
-from pytgcalls.types import MediaStream, AudioQuality, VideoQuality, Update
+from pytgcalls import PyTgCalls, StreamType
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from pytgcalls.types.input_stream.quality import (HighQualityAudio,
+                                                  HighQualityVideo,
+                                                  LowQualityAudio,
+                                                  LowQualityVideo,
+                                                  MediumQualityAudio,
+                                                  MediumQualityVideo)
 
 from config import OWNER, OWNER_NAME, VIDEO
 
@@ -130,6 +136,8 @@ async def admin_risght(client: Client, message):
        videoid = check[0]["videoid"]
        user_id = check[0]["user_id"]
        link = check[0]["link"]
+       audio_stream_quality = MediumQualityAudio()
+       video_stream_quality = MediumQualityVideo()
        if file:
          file_path = file
        else:     
@@ -137,7 +145,7 @@ async def admin_risght(client: Client, message):
             file_path = await download(bot_username, link, video)
          except:
             return client.send_message(chat_id, "**هەڵە ڕوویدا لە کاتی پەخشکردنی دواتر 🎻.**")
-       stream = (MediaStream(file_path, audio_parameters=AudioQuality.MEDIUM, video_parameters=VideoQuality.SD_480p) if video else MediaStream(file_path, audio_parameters=AudioQuality.MEDIUM))
+       stream = (AudioVideoPiped(file_path, audio_parameters=audio_stream_quality, video_parameters=video_stream_quality) if video else AudioPiped(file_path, audio_parameters=audio_stream_quality))
        try:
            await call.change_stream(chat_id, stream)
        except Exception:
