31eabb512c72c4b0edce038ec86bb932ee0b31fc
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-10-28 08:16:17 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,4 +1,4 @@
-OWNER = ["Hawaallll"]
+OWNER = ["Hawaallll", "IQ7amo"]
 OWNER_NAME = "𝐇𝐚𝐰𝐚𝐥𝐥🔥 ꨄᵉⁿᵈ"
 BOT_TOKEN = "7457379005:AAFGa-KqppzmKI1NuzW2sp2PUwJLmyC3wuE"
 DATABASE = "mongodb+srv://HawalRexar:r0sRuY8bxpQJr2x7@cluster0.l1tqcwu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
